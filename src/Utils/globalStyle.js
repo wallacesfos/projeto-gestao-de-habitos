6cbcc71bfabb94c41ccca5ad40facf220ad4a937
wallacesfos <wallacesfo@hotmail.com>
2021-10-15 17:02:08 -0300

    fix: Arrumado os erros

--- src/Utils/globalStyle.js
+++ src/Utils/globalStyle.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
-export const globalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css2?family=Red+Hat+Text:wght@300;400;600&display=swap');
     :root{
         --orange-main: #FF6400;
@@ -12,4 +12,36 @@ export const globalStyle = createGlobalStyle`
         --success: #019733;
         --blue: #2d4b94;
     }
+
+    body,div,main,section,ul,li,a,h1,h2,h3,h4,h5,h6{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        text-decoration: none;
+        list-style: none;
+    }
+
+    body{
+        background-color: var(--background)
+    }
+
+    .btn-primary{
+        background-color: #FF6400 !important;
+        border-radius: 15px;
+        width: 200px;
+        height: 33px;
+        border: 1px solid transparent;
+        -webkit-box-shadow: 7px 10px 12px -6px rgba(0,0,0,0.51); 
+        box-shadow: 7px 10px 12px -6px rgba(0,0,0,0.51);
+        color: white;
+        font-size: 15px;
+        font-weight: bold;
+        cursor: pointer;
+    }
+    
+    .btn-primary:hover{
+        background-color: rgb(255, 131, 45) !important;
+    }
+
+
 `;
