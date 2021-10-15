28eb2f5badc0e1ce6d1f1ce64aa389629e1fb8c0
Bruno <ferreira.bruno.oliveira@gmail.com>
2021-10-15 12:03:06 -0300

    feat: adicionando cores e fontes ao style global

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Cards from "./Components/Cards";
 
 function App() {
@@ -6,6 +7,13 @@ function App() {
       <Cards />
     </div>
   );
+=======
+import Input from "./components/Input";
+import "./Utils/globalStyle.js";
+
+function App() {
+  return <div className="App"></div>;
+>>>>>>> edf4a82 (feature: adicionando cores e fontes ao style global)
 }
 
 export default App;
--- src/Utils/globalStyle.js
+++ src/Utils/globalStyle.js
@@ -1,9 +1,15 @@
 import { createGlobalStyle } from "styled-components";
 
 export const globalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Red+Hat+Text:wght@300;400;600&display=swap');
     :root{
         --orange-main: #FF6400;
         --background: #F8EADA;
+        --background-escuro: #f2cfa5;
+        --color-placeHolder: #7f898d;
+        --color-cinzaEscuro: #37474F;
+        --danger : #b8293d;
+        --success: #019733;
+        --blue: #2d4b94;
     }
-
 `;
