2116cbadad4d51c47ea976ad15190255e3d844f1
wallacesfos <wallacesfo@hotmail.com>
2021-10-15 11:31:30 -0300

    Adicionado o GlobalStyle

---
+++ src/Utils/globalStyle.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+export const globalStyle = createGlobalStyle`
+    :root{
+        --orange-main: #FF6400;
+        --background: #F8EADA;
+    }
+
+`;
