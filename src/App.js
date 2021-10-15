edf4a829f087d5fd6160ace931c49624a3dec17b
Bruno <ferreira.bruno.oliveira@gmail.com>
2021-10-15 12:03:06 -0300

    feature: adicionando cores e fontes ao style global

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import Input from "./components/Input";
+import "./Utils/globalStyle.js";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
