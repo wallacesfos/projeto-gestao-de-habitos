db8437d34866ca0029602c63cf030232c22c2c0a
Etnan Sousa <poketnans@gmail.com>
2021-10-15 16:10:02 -0300

    feat: implementa o arquivo para auxiliar no teste dos endpoints

--- src/Utils/endpoints/test.js
+++ src/Utils/endpoints/test.js
@@ -1,5 +1,15 @@
-// import createAcount from "./user";
+// colocar o token aqui
+const token = "";
 
-// createAcount("poketeste", "poketeste@email.com", "123456").then((resp) =>
-//   console.log(resp)
-// );
+// Inserir as chaves do body
+const body = {};
+
+// inserir aqui os argumentos que a função pedir - caso peça um objeto
+const arg = {};
+
+export const teste = () => {
+  // importar e chamar a função aqui dentro exemplo:
+  // login(body)
+  //   .then((resp) => console.log(resp))
+  //   .catch((err) => console.log(err));
+};
