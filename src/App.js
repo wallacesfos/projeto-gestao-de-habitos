142a4f734d3af8f34f970fc061d938c9b9f84666
Wallace <wallacesfo@hotmail.com>
2021-10-18 19:27:40 -0300

    feat: Concluído o Schema e Yup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Register from './components/Register';
 import { GlobalStyle } from './Utils/globalStyle'
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle /> 
-      <Register />
     </div>
   );
 }
