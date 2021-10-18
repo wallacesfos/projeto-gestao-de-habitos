448da217d55d39548d9c064fc8455c0cee9cce4d
Bruno <ferreira.bruno.oliveira@gmail.com>
2021-10-18 13:05:41 -0300

    feature: button add

---
+++ src/Components/ButtonAdd/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import add from "../../Utils/Assets/add.svg";
+export const ButtonNew = styled.button`
+  background-color: transparent;
+  background-image: url(${add});
+  width: 150px;
+  height: 150px;
+  border: none;
+`;
