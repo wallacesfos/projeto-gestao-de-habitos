3f86a6ba773bcfb06f02747a97c3092730e5a545
Etnan Sousa <poketnans@gmail.com>
2021-10-18 12:44:57 -0300

    feat: inicia implementacao integraçao com endpoints

--- src/Components/PopupScreen/index.jsx
+++ src/Components/PopupScreen/index.jsx
@@ -1,6 +1,7 @@
 import group_members from "./../../Utils/Assets/group_members.png";
 import goal from "./../../Utils/Assets/goal.png";
 import event from "./../../Utils/Assets/event.png";
+import { useHistory } from "react-router-dom";
 
 import { CgClose } from "react-icons/cg";
 
@@ -18,9 +19,38 @@ import {
 import groupCategories from "../../Utils/groupCatecories";
 import stringNormalizer from "../../Utils/stringNormalizer";
 import stringCutter from "../../Utils/stringCutter";
+import {
+  getSpecificGroup,
+  subscribeToGroup,
+  unsubscribeFromGroup,
+} from "../../Utils/endpoints/groups";
+import jwtDecode from "jwt-decode";
+import { useEffect, useState } from "react";
 
-const PopupScreen = ({ group, setCloseState }) => {
-  const { name, description, users_on_group, goals, activities, category } =
+const PopupScreen = ({ group_id, setCloseState }) => {
+  const { push: goTo } = useHistory();
+
+  const [group, updateGroups] = useState([]);
+
+  const getGroup = (id) => {
+    let group = [];
+
+    getSpecificGroup(id)
+      .then(({ data }) => {
+        group = data;
+      })
+      .catch((err) => console.log(err));
+
+    return group;
+  };
+
+  getGroup(group_id);
+
+  // useEffect(() => {
+  console.log(getGroup(group_id));
+  // }, []);
+
+  const { id, name, description, users_on_group, goals, activities, category } =
     group;
 
   const ajustedname = stringCutter(name, 65);
@@ -36,8 +66,33 @@ const PopupScreen = ({ group, setCloseState }) => {
   const activitiesAmount = activities.length;
 
   const handleClose = () => setCloseState(false);
-  const handleMoreInfo = () => {};
-  const handleSubscribe = () => {};
+  const handleMoreInfo = () => goTo("/group-page");
+
+  // const token = JSON.parse(localStorage.getItem("@quero_token"))
+  const token =
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjM0NjkwNDA4LCJqdGkiOiI0NjNlNGQ1YzBmN2Y0YzQzOWQ4NTgxMmE0NDE3OTI5YSIsInVzZXJfaWQiOjQwfQ.5OIc_pRkftoZBoIqN0V6-dg-VNoveVXOz5F7pZHWlBw";
+
+  const { user_id } = jwtDecode(token);
+
+  const args = { group_id: id, token };
+  const alreadyOnGroup = users_on_group.some(({ id }) => id === user_id);
+
+  const handleSubscribe = () => {
+    if (alreadyOnGroup) {
+      unsubscribeFromGroup(args)
+        .then((resp) => console.log(resp))
+        .catch((err) => console.log(err));
+    }
+
+    subscribeToGroup(args)
+      .then((resp) => {
+        updateGroups(getSpecificGroup(group_id));
+        console.log(resp);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const buttonText = alreadyOnGroup ? "Sair do grupo" : "Se juntar ao grupo";
 
   return (
     <BackdropContainer>
@@ -70,7 +125,7 @@ const PopupScreen = ({ group, setCloseState }) => {
           </StatItem>
         </StatsBar>
 
-        <Button onClick={handleSubscribe}> Se juntar ao grupo </Button>
+        <Button onClick={handleSubscribe}> {buttonText} </Button>
       </PopupCard>
     </BackdropContainer>
   );
