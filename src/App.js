5d97492d21ac912a07223938dcec93f170c710ce
Wallace <73260587+wallacesfos@users.noreply.github.com>
2021-10-18 16:37:02 -0300

    Revert "Feature/yup"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { GlobalStyle } from './Utils/globalStyle'
+import { GlobalStyle } from "./Utils/globalStyle.js";
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyle /> 
+      <GlobalStyle />
     </div>
   );
 }
