bb61ba852d8b3cf8868ef85873872edc1ac5fa10
Etnan Sousa <poketnans@gmail.com>
2021-10-19 11:18:13 -0300

    refactor: refatora as funçoes de habitos
    Ajusta o codigo para retornar um objeto que facilite a tratativa de erros

--- src/Utils/endpoints/user/index.js
+++ src/Utils/endpoints/user/index.js
@@ -1,26 +1,9 @@
 import jwtDecode from "jwt-decode";
+import requisitionControler from "../../../Services/requisitionControler";
 import api from "../../api";
 
-const respConstructor = ({ data, status, statusText }) => {
-  return { data, status, statusText };
-};
-
-const requisitionControler = async (promise) => {
-  let resp = {};
-
-  await promise
-    .then((data) => {
-      resp = { ...respConstructor(data) };
-    })
-    .catch((err) => {
-      resp = { ...respConstructor(err.response) };
-    });
-
-  return resp;
-};
-
 export const createAcount = (body) => {
-  let resp = requisitionControler(
+  const resp = requisitionControler(
     api.post("users/", body, {
       headers: {
         "Content-Type": "application/json",
@@ -32,7 +15,7 @@ export const createAcount = (body) => {
 };
 
 export const login = (body) => {
-  let resp = requisitionControler(
+  const resp = requisitionControler(
     api.post("sessions/", body, {
       headers: {
         "Content-Type": "application/json",
@@ -46,7 +29,7 @@ export const login = (body) => {
 export const updateProfile = ({ body, token }) => {
   const { user_id } = jwtDecode(token);
 
-  let resp = requisitionControler(
+  const resp = requisitionControler(
     api.patch(`users/${user_id}/`, body, {
       headers: {
         "Content-Type": "application/json",
