004394fb017836cab2ae2774024e53f5fda5c5f9
Bruno <ferreira.bruno.oliveira@gmail.com>
2021-10-19 17:24:51 -0300

    fix: removed link header app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Header from "./Pages/Dashboard/DachboardHabits/index.jsx";
 import "./Utils/globalStyle.js";
 import { GlobalStyle } from "./Utils/globalStyle.js";
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <Header />
     </div>
   );
 }
