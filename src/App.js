6798344e9975fb04b1a60f7aece9954d222e3f47
Bruno <ferreira.bruno.oliveira@gmail.com>
2021-10-19 14:51:50 -0300

    feat: menu page habits

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Habits from "./Pages/Dashboard/DachboardHabits/index.jsx";
-import Home from "./Pages/Home";
 import "./Utils/globalStyle.js";
 import { GlobalStyle } from "./Utils/globalStyle.js";
 function App() {
@@ -7,7 +6,6 @@ function App() {
     <div className="App">
       <GlobalStyle />
       <Habits />
-      <Home />
     </div>
   );
 }
--- src/Pages/Dashboard/DachboardHabits/index.jsx
+++ src/Pages/Dashboard/DachboardHabits/index.jsx
@@ -1,1 +1,17 @@
-export default function Habits() {}
+import { Container } from "./styles.js";
+import logo from "../../../Utils/Assets/logo.png";
+import Nav from "../../../Components/Nav";
+import Search from "../../../Components/Search";
+export default function Habits() {
+  return (
+    <Container>
+      <div className="col-left">
+        <img src={logo} alt="Logo Quero!" />
+      </div>
+      <div className="col-right">
+        <Nav dashboard />
+        <Search />
+      </div>
+    </Container>
+  );
+}
