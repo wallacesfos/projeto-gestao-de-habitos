00e9ce793528f66a0b5712614be7237692cc8ae5
Etnan Sousa <poketnans@gmail.com>
2021-10-19 10:57:40 -0300

    refactor: refatora as funçoes User para async
    Implementa a funcionalidade async para que o retorno da funçao seja um objeto com as informaçoes uteis na tratativa de erro.

--- src/Utils/endpoints/user/index.js
+++ src/Utils/endpoints/user/index.js
@@ -1,29 +1,59 @@
 import jwtDecode from "jwt-decode";
 import api from "../../api";
 
+const respConstructor = ({ data, status, statusText }) => {
+  return { data, status, statusText };
+};
+
+const requisitionControler = async (promise) => {
+  let resp = {};
+
+  await promise
+    .then((data) => {
+      resp = { ...respConstructor(data) };
+    })
+    .catch((err) => {
+      resp = { ...respConstructor(err.response) };
+    });
+
+  return resp;
+};
+
 export const createAcount = (body) => {
-  return api.post("users/", body, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  let resp = requisitionControler(
+    api.post("users/", body, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+  );
+
+  return resp;
 };
 
 export const login = (body) => {
-  return api.post("sessions/", body, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  let resp = requisitionControler(
+    api.post("sessions/", body, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+  );
+
+  return resp;
 };
 
 export const updateProfile = ({ body, token }) => {
   const { user_id } = jwtDecode(token);
 
-  return api.patch(`users/${user_id}/`, body, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  let resp = requisitionControler(
+    api.patch(`users/${user_id}/`, body, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+  );
+
+  return resp;
 };
