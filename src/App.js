bcea826cbf5e6b861d4caa26a742352cf2d5d3b2
Bruno <ferreira.bruno.oliveira@gmail.com>
2021-10-19 14:26:45 -0300

    feat:  component footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import Habits from "./Pages/Dashboard/DachboardHabits/index.jsx";
-import Home from "./Pages/Home";
 import "./Utils/globalStyle.js";
 import { GlobalStyle } from "./Utils/globalStyle.js";
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <Habits />
-      <Home />
     </div>
   );
 }
