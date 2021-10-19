d51b76d20de44505db8f1868f7a21b3c8cb6b13e
Bruno <ferreira.bruno.oliveira@gmail.com>
2021-10-19 11:33:02 -0300

    feat: page habits incomplited

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import Search from "./Components/Search/index.jsx";
-import Subscribe from "./Pages/Subscribe/index.jsx";
+import Habits from "./Pages/DachboardHabits/index.jsx";
 import "./Utils/globalStyle.js";
 import { GlobalStyle } from "./Utils/globalStyle.js";
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <Subscribe />
+      <Habits />
     </div>
   );
 }
