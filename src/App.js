ec673d9f626b3aa23a153a73a033d74b713155eb
Wallace <wallacesfo@hotmail.com>
2021-10-20 09:33:46 -0300

    feat: Terminado formulario de Login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import SignIn from "./Components/Singnin/index.js";
 import { GlobalStyle } from "./Utils/globalStyle.js";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <SignIn />
     </div>
   );
 }
