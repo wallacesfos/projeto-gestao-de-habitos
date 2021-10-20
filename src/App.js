4faaa0a4bee93fff8f8ebd69ff098ffff1c68d94
Wallace <wallacesfo@hotmail.com>
2021-10-20 17:04:38 -0300

    Limpado o app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import { GlobalStyle } from "./Utils/globalStyle.js";
-import Home from "./Pages/Home";
-import Login from "./Pages/Login";
-import Subscribe from "./Pages/Subscribe/index.jsx";
 function App() {
   return (
     <div className="App">
