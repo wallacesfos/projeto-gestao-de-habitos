8f67f66e94832f3d853d02c7cde3dbd704309f27
Bruno <ferreira.bruno.oliveira@gmail.com>
2021-10-20 08:55:17 -0300

    fix: correcao de placeholder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from "./Components/HeaderDashboard/index.jsx";
 import { GlobalStyle } from "./Utils/globalStyle.js";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Header placeHolder="Digite o nome do habito..." />
     </div>
   );
 }
