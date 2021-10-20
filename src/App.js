fa1b7c89c60a35e4e31312467acf743c3c9002bb
Wallace <wallacesfo@hotmail.com>
2021-10-20 09:34:40 -0300

    Feat: Terminado o SignIn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import SignIn from "./Components/Singnin/index.js";
 import { GlobalStyle } from "./Utils/globalStyle.js";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <SignIn />
     </div>
   );
 }
