abfe094ba767515d2b655e48e53d5a676b32e021
Wallace <wallacesfo@hotmail.com>
2021-10-20 16:57:31 -0300

    Fix: Adicionado o history

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import Register from "./Components/Register";
-
 import { GlobalStyle } from "./Utils/globalStyle.js";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <Register />
     </div>
   );
 }
