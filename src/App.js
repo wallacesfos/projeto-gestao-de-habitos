2d9743c9026ad8de2d393d7eb126ffe6a732a2a7
Bruno <ferreira.bruno.oliveira@gmail.com>
2021-10-20 13:33:13 -0300

    fix: correct space link navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { GlobalStyle } from "./Utils/globalStyle.js";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 import Subscribe from "./Pages/Subscribe/index.jsx";
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Home />
+      <br />
+      <br />
+      <Login />
+      <br />
+      <br />
       <Subscribe />
     </div>
   );
