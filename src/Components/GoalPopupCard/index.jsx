7e18e600d719b21099fe9a303a14abf9400985dd
Etnan Sousa <poketnans@gmail.com>
2021-10-20 00:33:33 -0300

    feat: inicia implementaçao do popup de metas

---
+++ src/Components/GoalPopupCard/index.jsx
@@ -0,0 +1,80 @@
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { CgClose } from "react-icons/cg";
+
+import {
+  BackdropContainer,
+  CardHeader,
+  PopupCard,
+  CardDescription,
+  Button,
+} from "./styles";
+import {
+  deleteGoal,
+  getSpecificGoal,
+  updateGoal,
+} from "../../Utils/endpoints/goals";
+import { useEffect, useState } from "react";
+
+const GoalPopupCard = ({ goal_id, setCloseState, closeValue }) => {
+  const [goal, setGoal] = useState({});
+
+  useEffect(() => {
+    const getGoal = async () => {
+      let goal = {};
+      await getSpecificGoal(goal_id).then(({ data }) => setGoal(data));
+      return goal;
+    };
+    getGoal();
+  }, []);
+
+  const { title, difficulty, achieved, how_much_achieved, group } = goal;
+
+  const ajustedTitle = title;
+
+  const handleClose = () => setCloseState(closeValue);
+
+  const token = JSON.parse(localStorage.getItem("@Quero_token"));
+
+  const handleCheckin = () => {
+    const body = {
+      how_much_achieved: how_much_achieved + 1,
+    };
+
+    updateGoal({ goal_id, body, token })
+      .then(() => toast.success("Meta atualizada!"))
+      .catch((err) => {
+        toast.error("Deu ruim!");
+        console.log(err.response);
+      });
+  };
+  const hadleDelete = () => {
+    deleteGoal({ goal_id, token })
+      .then(() => toast.success("Meta deletada!"))
+      .catch((err) => {
+        toast.error("Deu ruim!");
+        console.log(err.response);
+      });
+    setTimeout(setCloseState(closeValue), 5000);
+  };
+
+  return (
+    <BackdropContainer>
+      <ToastContainer />
+      <PopupCard>
+        <CardHeader>
+          <p>{difficulty}</p>
+          <CgClose onClick={handleClose} />
+        </CardHeader>
+
+        <CardDescription>{ajustedTitle}</CardDescription>
+
+        <button onClick={hadleDelete}> deletar </button>
+
+        <Button onClick={handleCheckin}> Check-in </Button>
+      </PopupCard>
+    </BackdropContainer>
+  );
+};
+
+export default GoalPopupCard;
