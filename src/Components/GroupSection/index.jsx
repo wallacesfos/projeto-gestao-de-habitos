d83e0c799763539b8e7c8b6960b5641a5329475d
Etnan Sousa <poketnans@gmail.com>
2021-10-19 23:01:04 -0300

    feat: implementa componente base para construçao da pagina do grupo

--- src/Components/GroupSection/index.jsx
+++ src/Components/GroupSection/index.jsx
@@ -1,15 +1,27 @@
+import { SectionBox } from "./styles";
+
 const GroupCardSection = ({ variant, children }) => {
   const group_members = "";
   const goal = "";
   const event = "";
 
   const headerInfo = {
-    membros: { text: "Membros", img: group_members },
-    metas: { text: "Metas", img: goal },
-    atividades: { text: "Atividades", img: event },
+    members: { text: "Membros", img: group_members, color: "#E09CD1" },
+    goals: { text: "Metas", img: goal, color: "#B6E09C" },
+    activities: { text: "Atividades", img: event, color: "#9CE0CC" },
   };
 
-  return <></>;
+  const { text, img, color } = headerInfo[variant];
+
+  return (
+    <SectionBox {...{ color }}>
+      <div className="headerBox">
+        <img src={img} alt="Ícone membros" />
+        <h3> {text} </h3>
+      </div>
+      {children}
+    </SectionBox>
+  );
 };
 
 export default GroupCardSection;
