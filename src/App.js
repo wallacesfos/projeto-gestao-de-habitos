c3d1278274c507a8da1baed90e33fb3bfd8927c9
Wallace <wallacesfo@hotmail.com>
2021-10-21 10:30:00 -0300

    Fix: Arrumado o Token

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import Routes from "./Routes/";
+import Habits from "./Pages/Dashboard/DachboardHabits/index.jsx";
 import { GlobalStyle } from "./Utils/globalStyle.js";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <Routes />
+      <ToastContainer />
+      {/* <Login /> */}
+      <br />
+      <br />
+      <Habits />
+      {/* <FormAddHabits /> */}
     </div>
   );
 }
-
 export default App;
