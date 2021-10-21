ea0819a25998e9cfd2f60b9611eb5b48328e3f12
Wallace <wallacesfo@hotmail.com>
2021-10-21 10:31:15 -0300

    fix: arrumado o APP

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import Habits from "./Pages/Dashboard/DachboardHabits/index.jsx";
 import { GlobalStyle } from "./Utils/globalStyle.js";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import Routes from './Routes'
+
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <ToastContainer />
-      {/* <Login /> */}
-      <br />
-      <br />
-      <Habits />
-      {/* <FormAddHabits /> */}
+      <Routes />
     </div>
   );
 }
